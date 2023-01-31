751987adc71083f031803ecb3d7de423436952a4
Joey Yu <joey@itsjoeoui.com>
2023-01-30 23:25:02 -0500

    Getting started

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
-from flask import Flask
+from flask import Flask, jsonify
+import docker
 
 app = Flask(__name__)
+app.debug = True
+
+dc = docker.from_env()
+
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    containers = [c.short_id for c in dc.containers.list()]
+    return jsonify(containers)
+
+
+if __name__ == "__main__":
+    app.run(port=5555)
