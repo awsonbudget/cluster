750e58b48ae21106437f2ee9841c41eeb564c914
Joey Yu <joey@itsjoeoui.com>
2023-02-03 13:06:56 -0500

    Add pod POST and DELETE

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, Response
+from typing import Optional
 import docker
 
 app = Flask(__name__)
@@ -6,11 +7,34 @@ app.debug = True
 
 dc = docker.from_env()
 
+pods: dict = dict()
+
 
 @app.route("/")
 def hello_world():
     containers = [c.short_id for c in dc.containers.list()]
     return jsonify(containers)
+
+
+@app.route("/cloud/pod/<string:name>", methods=["POST", "DELETE"])
+def pod(name: Optional[str] = None) -> Response:
+    if request.method == "POST":
+        if name in pods:
+            return jsonify(
+                status=False, msg=f"cluster: {name} is already a pod in pods"
+            )
+        pods[name] = []
+        return jsonify(status=True, msg=f"cluster: {name} is added as a pod")
+
+    if request.method == "DELETE":
+        # TODO: check for instances before removing
+        rtn = pods.pop(name, False)
+
+        if rtn == False:
+            return jsonify(status=False, msg=f"cluster: {name} is not a pod in pods")
+        return jsonify(status=True, msg=f"cluster: {name} is removed from pods")
+
+    return jsonify(status=False, msg="cluster: what the hell is happenning")
 
 
 if __name__ == "__main__":
