220550839da2bb7083d5ba1a4a7be2895c43384f
Joey Yu <joey@itsjoeoui.com>
2023-03-19 10:54:03 -0400

    server baseline

---
+++ src/routers/server.py
@@ -0,0 +1,26 @@
+from fastapi import APIRouter, Depends, UploadFile
+from src.internal.cluster import Job
+from src.utils.config import cluster, dc, address
+from src.internal.auth import verify_setup
+from src.internal.type import Resp, NodeStatus, JobStatus
+
+
+router = APIRouter(tags=["server"])
+
+
+@router.post("/cloud/server/launch/", dependencies=[Depends(verify_setup)])
+async def server_launch(pod_id: str) -> Resp:
+    """cloud server launch POD_ID"""
+    return Resp(status=False)
+
+
+@router.post("/cloud/server/resume/", dependencies=[Depends(verify_setup)])
+async def server_resume(pod_id: str) -> Resp:
+    """cloud server resume PDO_ID"""
+    return Resp(status=False)
+
+
+@router.post("/cloud/server/pause/", dependencies=[Depends(verify_setup)])
+async def server_pause(pod_id: str) -> Resp:
+    """cloud server launch POD_ID"""
+    return Resp(status=False)
