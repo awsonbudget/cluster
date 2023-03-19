7f3572d1c2add4ec17e78234fb739650a9e63e6e
Joey Yu <joey@itsjoeoui.com>
2023-03-19 16:09:04 -0400

    Fix derp

--- src/routers/internal.py
+++ src/routers/internal.py
@@ -17,7 +17,7 @@ class Log(BaseModel):
 async def callback(job_id: str, node_id: str, exit_code: str, log: Log) -> Resp:
     # TODO: Handle None output
     assert address["manager"] != None
-    job = cluster.remove_running(job_id)
+    job = cluster.remove_running_job(job_id)
     if job == None:
         raise Exception(
             f"cluster: job {job_id} received from callback is not in the running list"
@@ -28,7 +28,7 @@ async def callback(job_id: str, node_id: str, exit_code: str, log: Log) -> Resp:
     if node == None:
         raise Exception(f"cluster: node {job.get_node_id()} does not exist")
     node.set_idle()
-    cluster.add_available_node(node)
+    cluster.add_available_job_node(node)
 
     with open(f"tmp/{node_id}/{job_id}.log", "w") as f:
         f.write(log.data if log.data else "")
@@ -44,4 +44,4 @@ async def callback(job_id: str, node_id: str, exit_code: str, log: Log) -> Resp:
 
 @router.get("/internal/available", dependencies=[Depends(verify_setup)])
 async def available() -> Resp:
-    return Resp(status=cluster.has_available_nodes())
+    return Resp(status=cluster.has_available_job_nodes())
