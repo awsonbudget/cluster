70f8a583f3b66d8af88f51976a5a78be2b0ca924
Joey Yu <joey@itsjoeoui.com>
2023-03-23 16:25:58 -0400

    Add a factorial example

--- example/express/app.js
+++ example/express/app.js
@@ -3,9 +3,9 @@ const app = express();
 const port = 3000;
 
 let count = 1;
+const argument = process.argv[2];
 
 app.get("/", (req, res) => {
-  const argument = process.argv[2];
   const msg = `Hello World! From ${argument}. This is request #${count}`;
   res.send(msg);
   console.log(msg);
@@ -14,6 +14,19 @@ app.get("/", (req, res) => {
 
 app.get("/ping", (req, res) => {
   res.send("pong!");
+});
+
+function factorial(n) {
+  if (n === 0) {
+    return 1;
+  }
+  return n * factorial(n - 1);
+}
+
+app.get("/factorial/:n", (req, res) => {
+  const n = parseInt(req.params.n);
+  const result = factorial(n);
+  res.send(`${argument}: The factorial of ${n} is ${result}`);
 });
 
 app.listen(port, () => {
